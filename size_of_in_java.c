591467a38e43d519212f9c5c8b8b76c1f1dab3ad
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-09-13 20:56:49 +0530

    Create size_of_in_java.c

---
+++ size_of_in_java.c
@@ -0,0 +1,15 @@
+class Main{
+    public static void main(String[]args)
+    {
+        int n;
+        float f;
+        double d;
+        char ch;
+        System.out.println("Size of int is "+ Integer.SIZE/8);
+        System.out.println("Size of float is "+Float.SIZE/8);
+        System.out.println("Size of double is "+Double.SIZE/8);
+        System.out.println("Size of Charater is " + Character.SIZE/8);
+        
+       
+}
+}
