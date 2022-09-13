b031289ffe72e0b64d62e0b09f895c6bc9b5c6bf
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-09-13 21:36:06 +0530

    Update calculator_using_switch_case.java

--- calculator_using_switch_case.java
+++ calculator_using_switch_case.java
@@ -2,23 +2,33 @@ import java.util.Scanner;
 class Main{
     public static void main(String[]args)
     {
-        Scanner input=new Scanner (System.in);
+        Scanner input=new Scanner(System.in);
         System.out.println("Enter the num1");
         int num1=input.nextInt();
         System.out.println("Enter the num2");
         int num2=input.nextInt();
-        System.out.println("enter 1 for additoin\n2 for substraction");
+        System.out.println("enter\n1=addition\n2=Substraction\n3=Multiplication\n4=Division\n5=Modulus");
         int choice=input.nextInt();
         switch(choice)
         {
             case 1:
-                System.out.println("Addition of number is "+(num1+num2));
+                System.out.println("Additon of number is "+(num1+num2));
                 break;
             case 2:
-                System.out.println("substraction of number is "+(num1-num2));
+                System.out.println("Substraction of number is "+(num1-num2));
+                break;
+            case 3:
+                System.out.println("Multiplication of number is "+(num1*num2));
+                break;
+            case 4:
+                System.out.println("Division of number is "+(num1/num2));
+                break;
+            case 5:
+                System.out.println("Modulus of number is "+(num1%num2));
                 break;
             default:
-                 System.out.println("enter correct choice");
+                System.out.println("Enter correct choice");
+                
         }
     }
 }
