18396849782bf4d77fed61c4ad7c9e7f4e4e0317
ashish200214 <64855416+ashish200214@users.noreply.github.com>
2022-12-20 18:04:56 -0800

    Update super_keyword.java

--- super_keyword.java
+++ super_keyword.java
@@ -47,3 +47,33 @@ class A {
         r.show();
     }
  }
+
+
+//for constructor
+/*super keyword :- It is used to differentiate the super class attribute,method and constructor  from the sub class
+ * when same name variable,method are used in in sub and parent class.
+*/
+
+//For default constructor it is not compalsory to write super keyword.
+//But for parameterised constructor it is compulsory to write super keyword otherwise it will give an error.
+class A {
+       A(int a) {
+        System.out.println("I am in A class constructor"+a);
+       }
+}
+
+ class B extends A {
+B() 
+{
+    super(10);  //automatically usedb or reserved by compiler
+    System.out.println("I am in B class constructor");
+
+} 
+}
+
+ class Test {
+    public static void main(String[] args) {
+        B r=new B();
+    
+    }
+ }
